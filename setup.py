9c2b8ac3dd003c98ae2565ccd33727156fe47c23
Carles Sala <carles@pythiac.com>
2020-05-23 02:38:41 +0200

    Bump version: 0.0.1.dev0 → 0.0.1

--- setup.py
+++ setup.py
@@ -99,6 +99,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/DataTracer',
-    version='0.0.1.dev0',
+    version='0.0.1',
     zip_safe=False,
 )
