0c850bce34f22f1df93736f3b71340f8faae8a77
Carles Sala <carles@pythiac.com>
2020-06-19 19:23:25 +0200

    Add unit tests for update_metad

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from metad import MetaData
 
@@ -106,13 +108,86 @@ def test_primary_keys(metad, primary_keys):
     assert updated.data['tables'][1]['primary_key'] == 'another_field'
 
 
+def test_primary_keys_dict(metad, primary_keys):
+    updated = metadata.update_metadata_primary_keys(metad.data, primary_keys)
+
+    assert updated.data['tables'][0]['primary_key'] == 'a_field'
+    assert updated.data['tables'][1]['primary_key'] == 'another_field'
+
+
+def test_primary_keys_str(metad, primary_keys, tmpdir):
+    metad_path = os.path.join(tmpdir, 'metadata.json')
+    metad.to_json(metad_path)
+    updated = metadata.update_metadata_primary_keys(metad_path, primary_keys)
+
+    assert updated.data['tables'][0]['primary_key'] == 'a_field'
+    assert updated.data['tables'][1]['primary_key'] == 'another_field'
+
+
+def test_primary_output_path(metad, primary_keys, tmpdir):
+    output_path = os.path.join(tmpdir, 'metadata.json')
+    metadata.update_metadata_primary_keys(metad, primary_keys, output_path=output_path)
+
+    updated = MetaData.from_json(output_path)
+    assert updated.data['tables'][0]['primary_key'] == 'a_field'
+    assert updated.data['tables'][1]['primary_key'] == 'another_field'
+
+
 def test_foreign_keys(metad, foreign_keys):
     updated = metadata.update_metadata_foreign_keys(metad, foreign_keys)
 
     assert updated.data['foreign_keys'] == foreign_keys
 
 
-def test_column_map(metad, column_map, constraints):
-    updated = metadata.update_metadata_column_map(metad, column_map, '1234', 'a_field')
+def test_foreign_keys_dict(metad, foreign_keys):
+    updated = metadata.update_metadata_foreign_keys(metad.data, foreign_keys)
 
+    assert updated.data['foreign_keys'] == foreign_keys
+
+
+def test_foreign_keys_str(metad, foreign_keys, tmpdir):
+    metad_path = os.path.join(tmpdir, 'metadata.json')
+    metad.to_json(metad_path)
+    updated = metadata.update_metadata_foreign_keys(metad_path, foreign_keys)
+
+    assert updated.data['foreign_keys'] == foreign_keys
+
+
+def test_foreign_keys_output_path(metad, foreign_keys, tmpdir):
+    output_path = os.path.join(tmpdir, 'updated.json')
+    metadata.update_metadata_foreign_keys(metad, foreign_keys, output_path=output_path)
+
+    updated = MetaData.from_json(output_path)
+    assert updated.data['foreign_keys'] == foreign_keys
+
+
+def test_column_map(metad, column_map, constraints):
+    updated = metadata.update_metadata_column_map(
+        metad, column_map, target_table='1234', target_field='a_field')
+
+    assert updated.data['constraints'] == constraints
+
+
+def test_column_map_dict(metad, column_map, constraints):
+    updated = metadata.update_metadata_column_map(
+        metad.data, column_map, target_table='1234', target_field='a_field')
+
+    assert updated.data['constraints'] == constraints
+
+
+def test_column_map_str(metad, column_map, constraints, tmpdir):
+    metad_path = os.path.join(tmpdir, 'metadata.json')
+    metad.to_json(metad_path)
+    updated = metadata.update_metadata_column_map(
+        metad_path, column_map, target_table='1234', target_field='a_field')
+
+    assert updated.data['constraints'] == constraints
+
+
+def test_column_map_output_path(metad, column_map, constraints, tmpdir):
+    output_path = os.path.join(tmpdir, 'metadata.json')
+    metadata.update_metadata_column_map(
+        metad, column_map, target_table='1234', target_field='a_field', output_path=output_path)
+
+    updated = MetaData.from_json(output_path)
     assert updated.data['constraints'] == constraints
