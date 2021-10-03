07de44562037691959248f8603b1bebf014926f6
Zhuofan Xie <zhuofanx@mit.edu>
2021-10-03 09:07:56 -0400

    Implemented composite primary key finder

---
+++ datatracer/primary_key_composite/__init__.py
@@ -0,0 +1,7 @@
+from datatracer.primary_key_composite.base import CompositePrimaryKeySolver
+from datatracer.primary_key_composite.basic import BasicCompositePrimaryKeySolver
+
+__all__ = [
+    "CompositePrimaryKeySolver",
+    "BasicCompositePrimaryKeySolver"
+]
---
+++ datatracer/primary_key_composite/base.py
@@ -0,0 +1,31 @@
+"""Primary Key Solving base class."""
+
+
+class CompositePrimaryKeySolver():
+
+    def fit(self, dict_of_databases):
+        """Fit this solver.
+
+        Args:
+            dict_of_databases (dict):
+                Map from database names to tuples containing ``MetaData``
+                instances and table dictionaries, which contain table names
+                as input and ``pandas.DataFrames`` as values.
+        """
+
+    def solve(self, tables):
+        """Solve the primary key detection problem.
+
+        The output is a dictionary contiaining table names as keys, and the
+        name of the field that is most likely to be the primary key as values.
+
+        Args:
+            tables (dict):
+                Dict containing table names as input and ``pandas.DataFrames``
+                as values.
+
+        Returns:
+            dict:
+                Dict containing table names as keys and list of field names as values.
+        """
+        raise NotImplementedError()
